264f8105e405182b13e46b8dbf50306dc5e0c376
jianjian <jja8@jja8.cn>
2023-08-14 19:15:37 +0800

    修改

--- CommunicationTypes/src/main/java/cn/jjaw/ktg8/type/core/HandshakeMessageServer.java
+++ CommunicationTypes/src/main/java/cn/jjaw/ktg8/type/core/HandshakeMessageServer.java
@@ -1,14 +1,11 @@
 package cn.jjaw.ktg8.type.core;
 
-import cn.jjaw.ktg8.censor.checker.AllNotNull;
-
 /**
  * 服务端握手
  * @param version 版本
  * @param serverID 服务器id
  * @param serverType 服务器类型
  */
-@AllNotNull
 public record HandshakeMessageServer (
     long version,
     String serverID,
