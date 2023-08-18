0b368d3228d219ae72f1277904f79adb65d0a265
jianjian <jja8@jja8.cn>
2023-08-18 15:19:23 +0800

    创建spigot插件，并配置好nms环境

---
+++ KnapsackToGo8-Spigot/src/main/java/cn/jjaw/ktg8/spigot/KTG8.java
@@ -0,0 +1,17 @@
+package cn.jjaw.ktg8.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KTG8 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
