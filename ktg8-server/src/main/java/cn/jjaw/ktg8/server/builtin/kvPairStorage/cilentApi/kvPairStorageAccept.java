b4e43930faf626dc91994d37e06384a8f90a41bb
简简爱 <2801819967@qq.com>
2023-09-11 02:42:36 +0000

    getPairStorage

--- ktg8-server/src/main/java/cn/jjaw/ktg8/server/builtin/kvPairStorage/cilentApi/kvPairStorageAccept.java
+++ ktg8-server/src/main/java/cn/jjaw/ktg8/server/builtin/kvPairStorage/cilentApi/kvPairStorageAccept.java
@@ -1,30 +1,50 @@
 package cn.jjaw.ktg8.server.builtin.kvPairStorage.cilentApi;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 
 import cn.jjaw.ktg8.server.api.Client;
-import cn.jjaw.ktg8.server.api.KTG8Plugin;
 import cn.jjaw.ktg8.server.api.RequestAccept;
 import cn.jjaw.ktg8.server.builtin.kvPairStorage.KvPairStorage;
+import cn.jjaw.ktg8.server.builtin.kvPairStorage.api.PairStorage;
 import cn.jjaw.ktg8.server.builtin.kvPairStorage.api.PairStorageManager;
 
 public class kvPairStorageAccept{
 	private final PairStorageManager pairStorageManager;
 	private final KvPairStorage kvPairStorage;
-	private final RequestAccept getStorageList;
+
+	private final RequestAccept pairStorages;
+	private final RequestAccept getPairStorage;
+
+	private final Map<String,PairStorage> storageKeyMap = new ConcurrentHashMap<>();
 	public kvPairStorageAccept(KvPairStorage ktg8Plugin) {
 		kvPairStorage = ktg8Plugin;
 		pairStorageManager = ktg8Plugin.getPairStorageManager();
-		getStorageList = new RequestAccept(ktg8Plugin, "getStorageList").setWorker(this::onGetStorageList).start();
+		pairStorages = new RequestAccept(ktg8Plugin, "pairStorages").setWorker(this::pairStorages).start();
+		getPairStorage = new RequestAccept(ktg8Plugin, "getPairStorage").setWorker(this::getPairStorage).start();
 	}
 
 
-	private JSONObject onGetStorageList(Client client,JSONObject requestData){
+	private JSONObject pairStorages(Client client,JSONObject requestData){
+		// TODO 没有测试
 		ArrayList list = new ArrayList<>(pairStorageManager.pairStorages());
 		return JSONObject.from(list);
+	}
+
+	private JSONObject getPairStorage(Client client,JSONObject requestData){
+		String storageName = requestData.getString("storageName");
+		PairStorage pairStorage = storageKeyMap.computeIfAbsent(storageName, key->pairStorageManager.getPairStorage(key));
+		boolean ok = false;
+		if(pairStorage!=null){
+			ok = true;
+		}
+		JSONObject json = new JSONObject();
+		json.put("ok",ok);
+		return json;
 	}
     
     
