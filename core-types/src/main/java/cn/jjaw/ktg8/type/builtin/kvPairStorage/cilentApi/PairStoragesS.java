c111fd52b1ab05f31197635f9d69ee4d0360d9c3
jjaw <jjaw@jjaw.cn>
2023-10-02 18:51:09 +0800

    使用更好更方便的工具

---
+++ core-types/src/main/java/cn/jjaw/ktg8/type/builtin/kvPairStorage/cilentApi/PairStoragesS.java
@@ -0,0 +1,4 @@
+package cn.jjaw.ktg8.type.builtin.kvPairStorage.cilentApi;
+
+public record PairStoragesS() {
+}
--- ktg8-server/src/main/java/cn/jjaw/ktg8/server/builtin/kvPairStorage/cilentApi/kvPairStorageAccept.java
+++ ktg8-server/src/main/java/cn/jjaw/ktg8/server/builtin/kvPairStorage/cilentApi/kvPairStorageAccept.java
@@ -5,59 +5,60 @@ import cn.jjaw.ktg8.server.builtin.kvPairStorage.KvPairStorageApi;
 import cn.jjaw.ktg8.server.builtin.kvPairStorage.PairStorage;
 import cn.jjaw.ktg8.server.builtin.kvPairStorage.PairStorageManager;
 import cn.jjaw.ktg8.server.core.Client;
-import cn.jjaw.ktg8.server.util.RequestAccept;
+import cn.jjaw.ktg8.server.util.RecordRequestAccept;
 import cn.jjaw.ktg8.type.builtin.kvPairStorage.cilentApi.*;
-import com.alibaba.fastjson2.JSONObject;
-import static cn.jjaw.ktg8.type.builtin.kvPairStorage.cilentApi.RequestName.*;
 
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static cn.jjaw.ktg8.type.builtin.kvPairStorage.cilentApi.RequestName.*;
+
 public class kvPairStorageAccept{
 	private final PairStorageManager pairStorageManager;
 	private final KvPairStorage kvPairStorage;
 
-	private final RequestAccept pairStorages;
-	private final RequestAccept getPairStorage;
-	private final RequestAccept createPairStorage;
+	private final RecordRequestAccept<PairStoragesS,PairStoragesC> pairStorages;
+	private final RecordRequestAccept<GetPairStorageS,GetPairStorageC> getPairStorage;
+	private final RecordRequestAccept<CreatePairStorageS,CreatePairStorageC> createPairStorage;
 
 	private final Map<String,PairStorage> storageKeyMap = new ConcurrentHashMap<>();
 	public kvPairStorageAccept(KvPairStorage ktg8Plugin) {
 		kvPairStorage = ktg8Plugin;
 		pairStorageManager = KvPairStorageApi.getPairStorageManager();
-		pairStorages = new RequestAccept(ktg8Plugin, PairStorages).setWorker(this::pairStorages).start();
-		getPairStorage = new RequestAccept(ktg8Plugin, GetPairStorage).setWorker(this::getPairStorage).start();
-		createPairStorage = new RequestAccept(ktg8Plugin,CreatePairStorage).setWorker(this::createPairStorage).start();
+		pairStorages = new RecordRequestAccept<>(ktg8Plugin, PairStorages, PairStoragesS.class,PairStoragesC.class)
+				.setWorker(this::pairStorages).start();
+		getPairStorage = new RecordRequestAccept<>(ktg8Plugin, GetPairStorage, GetPairStorageS.class, GetPairStorageC.class)
+				.setWorker(this::getPairStorage).start();
+		createPairStorage = new RecordRequestAccept<>(ktg8Plugin,CreatePairStorage, CreatePairStorageS.class, CreatePairStorageC.class)
+				.setWorker(this::createPairStorage).start();
 	}
 
 
 	/**
 	 * 获取其库存列表
 	 */
-	private JSONObject pairStorages(Client client,JSONObject requestData){
+	private PairStoragesC pairStorages(Client client,PairStoragesS data){
 		ArrayList<String> list = new ArrayList<>(pairStorageManager.pairStorages());
-		return JSONObject.from(new PairStoragesC(list));
+		return new PairStoragesC(list);
 	}
 
 	/**
 	 * 加载指定库存
 	 */
-	private JSONObject getPairStorage(Client client,JSONObject requestData){
-		GetPairStorageS request = requestData.to(GetPairStorageS.class);
+	private GetPairStorageC getPairStorage(Client client,GetPairStorageS request){
 		PairStorage pairStorage = storageKeyMap.computeIfAbsent(request.storageName(), pairStorageManager::getPairStorage);
 		boolean ok = pairStorage!=null;
-		return JSONObject.from(new GetPairStorageC(ok));
+		return new GetPairStorageC(ok);
 	}
 
 	/**
 	 * 创建指定库存，如果已经存在则加载
 	 */
-	private JSONObject createPairStorage(Client client,JSONObject requestData){
-		CreatePairStorageS request = requestData.to(CreatePairStorageS.class);
+	private CreatePairStorageC createPairStorage(Client client,CreatePairStorageS request){
 		PairStorage pairStorage = storageKeyMap.computeIfAbsent(request.storageName(), pairStorageManager::createPairStorage);
 		boolean ok = pairStorage!=null;
-		return JSONObject.from(new CreatePairStorageC(ok));
+		return new CreatePairStorageC(ok);
 	}
     
     
